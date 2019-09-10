18e7266580ffe81e5fe1c5821b65ea0e7e00b462
Gerson Espinal <gerson.espinal@gmail.com>
2019-09-09 22:12:47 -0400

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Game Week 5</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
