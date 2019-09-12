d0edc9cd6d3198b638fc9b2741c9df0ebfd18f59
Gerson Espinal <gerson.espinal@gmail.com>
2019-09-12 16:39:26 -0400

    Add firebase features

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Matches from "./components/Matches";
 import Players from "./components/Players";
 import LiveWatch from "./components/LiveWatch";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Route exact path="/" component={Matches} />
-      <Route exact path="/players" component={Players} />
-      <Route exact path="/livewatch" component={LiveWatch} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Matches} />
+        <Route exact path="/players" component={Players} />
+        <Route exact path="/livewatch" component={LiveWatch} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/LiveWatch.js
+++ client/src/components/LiveWatch.js
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 class LiveWatch extends Component {
   componentDidMount() {
     document.getElementById("livewatch").style.opacity = "1";
+    document.getElementById("livewatch").style.borderBottom =
+      "3px solid #8879f2";
   }
 
   componentWillUnmount() {
     document.getElementById("livewatch").style.opacity = ".7";
+    document.getElementById("livewatch").style.borderBottom =
+      "3px solid transparent";
   }
 
   render() {
