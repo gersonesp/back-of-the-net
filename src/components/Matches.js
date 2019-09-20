14355f564d29cdc129d13b00fbf1c19ca22ae683
Gerson Espinal <gerson.espinal@gmail.com>
2019-09-20 14:59:04 -0400

    Add populate state with input updates

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -5,30 +5,81 @@ class Matches extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      matches: []
+      predictions: {}
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  componentDidMount() {
+    let data = this.props.matches;
+
+    return Object.keys(data).map(match => {
+      return data[match].map(team => {
+        return this.setState({
+          [`${team.homeTeam}-${team.day}`]: 0,
+          [`${team.awayTeam}-${team.day}`]: 0
+        });
+      });
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+    document.querySelector(".matchesList").reset();
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+    this.setState({
+      [event.target.name]: value
+    });
+  }
+
+  increase(event) {
+    this.setState({
+      [`${event.target.name}`]: this.state[`${event.target.name}`] + 1
+    });
+  }
+
+  decrease(event) {
+    this.setState({
+      [`${event.target.name}`]: this.state[`${event.target.name}`] - 1
+    });
   }
 
   render() {
     const listOfMatches = this.props.matches;
+    console.log(this.state);
 
     return (
-      <form className="matchesList" onSubmit={this.handleSubmit}>
-        <h4 className="matchesHeader">Gameweek</h4>
+      this.state && (
+        <form className="matchesList" onSubmit={this.handleSubmit}>
+          <div className="matchesHeader">Gameweek</div>
 
-        <div className="listAllMatches">
-          {Object.keys(listOfMatches).map((match, i) => (
-            <div className="oneMatch" key={i}>
-              <div id="day">{match}</div>
-              <MatchDay matchDayList={listOfMatches[match]} />
-            </div>
-          ))}
-        </div>
+          <div className="listAllMatches">
+            {Object.keys(listOfMatches).map((match, i) => (
+              <div className="oneMatch" key={i}>
+                <div id="day">{match}</div>
+                <MatchDay
+                  matchDayList={listOfMatches[match]}
+                  handleInput={this.handleChange}
+                  state={this.state}
+                  increase={this.increase}
+                  decrease={this.decrease}
+                />
+              </div>
+            ))}
+          </div>
 
-        <button type="submit" value="Submit" id="button">
-          Submit
-        </button>
-      </form>
+          <button type="submit" value="Submit" id="button">
+            Submit
+          </button>
+        </form>
+      )
     );
   }
 }
--- src/components/matchDay.js
+++ src/components/matchDay.js
@@ -7,12 +7,58 @@ export default props => {
     <div id="teamContainer" key={match.idd}>
       <label id="homeTeam">
         <div className="teamName">{match.homeTeam}</div>
-        <input />
+
+        <div>
+          <button
+            name={`${match.homeTeam}-${match.day}`}
+            onClick={props.decrease}
+          >
+            -
+          </button>
+          <input
+            id={`${match.homeTeam}-${match.day}`}
+            value={props.state[`${match.homeTeam}-${match.day}`] || 0}
+            name={`${match.homeTeam}-${match.day}`}
+            onChange={props.handleInput}
+            type="number"
+            min="0"
+            max="10"
+          />
+          <button
+            name={`${match.homeTeam}-${match.day}`}
+            onClick={props.increase}
+          >
+            +
+          </button>
+        </div>
       </label>
 
       <label id="awayTeam">
         <div className="teamName">{match.awayTeam}</div>
-        <input />
+
+        <div>
+          <button
+            name={`${match.awayTeam}-${match.day}`}
+            onClick={props.decrease}
+          >
+            -
+          </button>
+          <input
+            id={`${match.awayTeam}-${match.day}`}
+            value={props.state[`${match.awayTeam}-${match.day}`] || 0}
+            name={`${match.awayTeam}-${match.day}`}
+            onChange={props.handleInput}
+            type="number"
+            min="0"
+            max="10"
+          />
+          <button
+            name={`${match.awayTeam}-${match.day}`}
+            onClick={props.increase}
+          >
+            +
+          </button>
+        </div>
       </label>
     </div>
   ));
